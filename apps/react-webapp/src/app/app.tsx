0cbab59f925d30fcdb598bc0b6da0810a269d80f
thorby <richardt@fotofactory.dk>
2023-09-14 20:19:16 +0200

    big time revamp

---
+++ apps/react-webapp/src/app/app.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import { useState, useEffect } from "react";
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  // This request is proxied to the backend
+  useEffect(() => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        const date = new Date(0);
+        setCurrentTime(date.setUTCSeconds(data.time));
+      });
+  }, []);
+
+  return (
+    <div style={{background: "green", color: "white"}}>
+      <p>The current date is '{new Date(currentTime).toString()}'.</p>
+    </div>
+  );
+}
+
+export default App;
